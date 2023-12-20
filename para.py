1c64ab0088704ab1b4ed32cc93e2e605f55924ff
Uchiha-Rin <64003021+Uchiha-Rin@users.noreply.github.com>
2023-12-20 17:12:00 +0800

    Add files via upload

---
+++ para.py
@@ -0,0 +1,11 @@
+input_window = 96
+output_window = 336
+
+model_save_path = 'model_longSeq.pt'
+loss_save_path = 'data_longSeq/loss'
+plot_save_path = 'data_longSeq/'
+test_save_path = 'data_longSeq/test'
+
+batch_size = 32
+lr = 0.00001
+epochs = 100
